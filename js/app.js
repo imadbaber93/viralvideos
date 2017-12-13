95fd904c421ec3c317938899164f060bb6709048
imadbaber93 <imadbaber@yahoo.com>
2017-12-12 19:40:08 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -33,7 +33,7 @@ function resetVideoHeight() {
 }
 
 function init() {
-    gapi.client.setApiKey("YOUR_PUBLIC_KEY");
+    gapi.client.setApiKey("AIzaSyA388q_XO6_LZrxMTfw41reoBplb--6tds");
     gapi.client.load("youtube", "v3", function() {
         // yt api is ready
     });
